98bc2e208b04f2b95ae76765c0d6282791229f9c
unknown <hartwellconnor@gmail.com>
2023-07-15 19:24:44 +0100

    feat: cleaned up event listener

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, abort
+import ngrok
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return "<p></p>"
 
-    return "<p>HEY WORLD</p>"
+
+@app.route("/webhook", methods=['GET'])
+def strava_webhook():
+    if request.args.get("hub.verify_token") == "STRAVA":
+        return {"hub.challenge": request.args.get("hub.challenge")}
+    else:
+        return abort(401, description="Could not verify token from Strava API: ")
+
+
+@app.route("/webhook", methods=['POST'])
+def strava_event_listener():
+    return {}
